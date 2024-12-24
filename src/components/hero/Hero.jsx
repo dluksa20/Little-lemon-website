1b34f628755077fc32d79c6424ed1dc83500b1a6
Dovydas Luksa <dluksa20@gmail.com>
2024-12-24 00:50:16 +0000

    first commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,33 +4,21 @@ import dish1 from '../../assets/images/dish1.jpg';
 
 const Hero = () => {
   return (
-    <div className="hero-container">
-
+    <div className="hero-background">
       <div className='hero-main'>
-        <div className='hero-left'>
-          <div className='hero-about'>
-            <div>
-              <h1>Little Lemon</h1>
-              <h2>Chicago</h2>
-              <p>We are a family-owned Mediterranean restaurant focused on traditional recipes 
+        <div className='hero-about'>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <br />
+          <p>We are a family-owned Mediterranean restaurant focused on traditional recipes 
                 served with a modern twist. Our roots trace back to generations of chefs who 
                 have perfected the art of Mediterranean cooking, using only the freshest and finest 
-                ingredients to bring you an unforgettable dining experience.</p>
-            </div>
-            <div className='hero-button'>
-              <button>Book Now</button>
-            </div>
+                ingredients to bring you an unforgettable dining experience.
+          </p>
 
-          </div>
-
-        </div>
-
-        <div className='hero-right'>
-          <div className='hero-slider'>
-            <img src={dish1} alt="" />
-          </div>
         </div>
       </div>
+
     </div>
   );
 };
